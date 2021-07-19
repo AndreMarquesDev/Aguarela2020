4d667d0aa2b3b239a91375b9f80951b47ab6a02f
AndreMarquesDev <andregm_7@hotmail.com>
2021-07-19 12:05:15 +0100

    Add default cypress files

---
+++ cypress/integration/home_page_spec.js
@@ -0,0 +1,12 @@
+describe('The Home Page', () => {
+    it('successfully loads', () => {
+        cy.visit('/');
+        /* ==== Generated with Cypress Studio ==== */
+        cy.get('ul.jsx-736606636 > :nth-child(1) > .jsx-736606636').click();
+        cy.get('ul.jsx-736606636 > :nth-child(1) > .jsx-736606636').click();
+        cy.get('.textBlock > .buttonWrapper > .buttonBackground > .jsx-2742012360').click();
+        cy.get('[alt="avocado logo"]').click();
+        cy.get('.wrapper > .buttonWrapper > .buttonBackground > .jsx-2742012360').click();
+        /* ==== End Cypress Studio ==== */
+    });
+});
