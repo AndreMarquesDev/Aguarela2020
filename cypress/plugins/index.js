288b1db11e1aebc5fb3c96ee7d2bdc1fa2c851d0
AndreMarquesDev <andregm_7@hotmail.com>
2021-07-19 17:18:09 +0100

    Add changes after rebase

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,22 +1,22 @@
 /// <reference types="cypress" />
 // ***********************************************************
-// This example plugins/index.js can be used to load plugins
+// this example plugins/index.js can be used to load plugins
 //
-// You can change the location of this file or turn off loading
+// you can change the location of this file or turn off loading
 // the plugins file with the 'pluginsFile' configuration option.
 //
-// You can read more here:
+// you can read more here:
 // https://on.cypress.io/plugins-guide
 // ***********************************************************
 
-// This function is called when a project is opened or re-opened (e.g. due to
+// this function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
 /**
  * @type {Cypress.PluginConfig}
  */
-// eslint-disable-next-line no-unused-vars
+// eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars, id-length
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-}
+    // `on` is used to hook into various events Cypress emits
+    // `config` is the resolved Cypress config
+};
