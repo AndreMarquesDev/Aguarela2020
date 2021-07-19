6c0195fb565fe55de4e0bf1ccd24ca2a66674a3b
AndreMarquesDev <andregm_7@hotmail.com>
2021-07-19 16:16:25 +0100

    Add Production pipeline

--- utils/formValidation.ts
+++ utils/formValidation.ts
@@ -15,12 +15,14 @@ export interface FormState {
 }
 
 export const validateName = (value: string): boolean => {
+    // eslint-disable-next-line no-useless-escape
     const nameRegex = /s*([a-zA-ZáâàãéêèíîìóôòõúûùñçäöüßÁÂÀÃÉÊÈÍÎÌÓÔÒÕÚÛÙÑÇÄÖÜẞ]{1,}([\.,] |[-']| ))+[a-zA-ZáâàãéêèíîìóôòõúûùñçäöüßÁÂÀÃÉÊÈÍÎÌÓÔÒÕÚÛÙÑÇÄÖÜẞ]+\.?\s*$/;
 
     return nameRegex.test(value);
 };
 
 export const validateEmail = (value: string): boolean => {
+    // eslint-disable-next-line no-useless-escape
     const emailRegex = /(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     return emailRegex.test(value);
