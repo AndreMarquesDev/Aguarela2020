093a988a34374fb0457beae09a4520fe06f7f904
AndreMarquesDev <andregm_7@hotmail.com>
2021-12-07 00:01:55 +0000

    feat: Add ProjectsList unit test

--- utils/jest/jestSetup.ts
+++ utils/jest/jestSetup.ts
@@ -1,3 +1,11 @@
 import { setJestWindowWidth } from './setJestWindowWidth';
 
+Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation(query => ({
+        matches: false,
+        media: query,
+    })),
+});
+
 setJestWindowWidth();
