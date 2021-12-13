7504e6c26f0a99adf82a840c93c7a58ebc82ccea
AndreMarquesDev <andregm_7@hotmail.com>
2021-12-13 22:52:36 +0000

    feat: Add ProjectsListDouble unit test

---
+++ components/ProjectsListDouble/ProjectsListDouble.test.tsx
@@ -0,0 +1,112 @@
+import '@testing-library/jest-dom';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { textsPt, textsEn } from '../../utils/texts';
+import TextsContext from '../context/TextsContext';
+import { setJestWindowWidth } from '../../utils/jest/setJestWindowWidth';
+import { ProjectsListDouble } from './ProjectsListDouble';
+import { Breakpoint } from '../../utils/useWindowSize';
+
+describe('<ProjectsListDouble />', () => {
+    test('renders properly', () => {
+        const { container } = render(<ProjectsListDouble />);
+
+        const title = screen.getByText(textsPt.projects);
+        const tjela = screen.getByAltText('tjela');
+        const tasteOfIndia = screen.getByAltText('taste of india');
+        const kaffeehaus = screen.getByAltText('kaffeehaus');
+        const avocado = screen.getByAltText('avocado');
+        const guacamole = screen.getByAltText('guacamole');
+        const mariaLimao = screen.getByAltText('marialimao');
+        const jameson = screen.getByAltText('jameson');
+        const beefeater = screen.getByAltText('beefeater');
+        const biergarten = screen.getByAltText('biergarten');
+        const madMary = screen.getByAltText('mad mary');
+        const bovine = screen.getByAltText('bovine');
+        const icecreamRoll = screen.getByAltText('icecream roll');
+        const riceMe = screen.getByAltText('rice me');
+        const riceMeDeli = screen.getByAltText('rice me deli');
+        const harpoon = screen.getByAltText('harpoon');
+        const catarinaSantiago = screen.getByAltText('catarina santiago');
+        const quatroPatasDeCincoEstrelas = screen.getByAltText('4 patas de 5 estrelas');
+        const luminous = screen.getByAltText('luminous');
+        const aAmigaEsteticista = screen.getByAltText('a amiga esteticista');
+        const AnaRo = screen.getByAltText('AnaRo');
+
+        const socialMediaAndContentCreationDescription = screen.getAllByText(
+            textsPt.socialMediaAndContentCreation
+        );
+        const socialMediaManagementDescription = screen.getAllByText(textsPt.socialMediaManagement);
+        const contentCreationDescription = screen.getAllByText(textsPt.contentCreation);
+        const socialMediaAndPaidSocialDescription = screen.getAllByText(
+            textsPt.socialMediaAndPaidSocial
+        );
+        const consultingAndContentCreationDescription = screen.getAllByText(
+            textsPt.consultingAndContentCreation
+        );
+
+        expect(title).toBeInTheDocument();
+        expect(tjela).toBeInTheDocument();
+        expect(tasteOfIndia).toBeInTheDocument();
+        expect(kaffeehaus).toBeInTheDocument();
+        expect(avocado).toBeInTheDocument();
+        expect(guacamole).toBeInTheDocument();
+        expect(mariaLimao).toBeInTheDocument();
+        expect(jameson).toBeInTheDocument();
+        expect(beefeater).toBeInTheDocument();
+        expect(biergarten).toBeInTheDocument();
+        expect(madMary).toBeInTheDocument();
+        expect(bovine).toBeInTheDocument();
+        expect(icecreamRoll).toBeInTheDocument();
+        expect(riceMe).toBeInTheDocument();
+        expect(riceMeDeli).toBeInTheDocument();
+        expect(harpoon).toBeInTheDocument();
+        expect(catarinaSantiago).toBeInTheDocument();
+        expect(quatroPatasDeCincoEstrelas).toBeInTheDocument();
+        expect(luminous).toBeInTheDocument();
+        expect(aAmigaEsteticista).toBeInTheDocument();
+        expect(AnaRo).toBeInTheDocument();
+
+        expect(socialMediaAndContentCreationDescription.length).toBe(13);
+        expect(socialMediaManagementDescription.length).toBe(3);
+        expect(contentCreationDescription.length).toBe(1);
+        expect(socialMediaAndPaidSocialDescription.length).toBe(1);
+        expect(consultingAndContentCreationDescription.length).toBe(2);
+
+        expect(container).toMatchSnapshot();
+    });
+
+    test('renders properly in English', () => {
+        const { container } = render(
+            <TextsContext.Provider
+                value={{
+                    texts: textsEn,
+                }}
+            >
+                <ProjectsListDouble />
+            </TextsContext.Provider>
+        );
+
+        const title = screen.getByText(textsEn.projects);
+
+        expect(title).toBeInTheDocument();
+
+        expect(container).toMatchSnapshot();
+    });
+
+    test('renders properly on phablet', () => {
+        setJestWindowWidth(Breakpoint.Phablet);
+
+        const { container } = render(<ProjectsListDouble />);
+
+        expect(container).toMatchSnapshot();
+    });
+
+    test('renders properly on mobile', () => {
+        setJestWindowWidth(Breakpoint.Mobile);
+
+        const { container } = render(<ProjectsListDouble />);
+
+        expect(container).toMatchSnapshot();
+    });
+});
---
+++ components/ProjectsListDouble/ProjectsListDouble.tsx
@@ -0,0 +1,261 @@
+import React, { FC, useContext } from 'react';
+import TextsContext from '../context/TextsContext';
+import Title from '../Title/Title';
+import { useWindowSize, Breakpoint } from '../../utils/useWindowSize';
+import NukaCarousel from '../NukaCarousel';
+import ProjectItem from '../ProjectItem';
+
+export const ProjectsListDouble: FC = () => {
+    const { texts } = useContext(TextsContext);
+    const windowSize = useWindowSize();
+    const isPhablet = windowSize.width < Breakpoint.Phablet;
+    const isMobile = windowSize.width < Breakpoint.Mobile;
+    // eslint-disable-next-line no-nested-ternary
+    const slidesToShow = isMobile ? 1 : isPhablet ? 2 : 3;
+
+    return (
+        <>
+            <section className="container">
+                <div className="wrapper genericMargins">
+                    <Title colored text={texts.projects} />
+
+                    <NukaCarousel
+                        cellAlign="left"
+                        slidesToShow={slidesToShow}
+                        width={isMobile ? '100%' : '90%'}
+                    >
+                        <div>
+                            <ProjectItem
+                                isActive
+                                isGrid
+                                isInPartnership
+                                brandLink="https://www.instagram.com/tudonatjela"
+                                brandTag="@tudonatjela"
+                                description={texts.socialMediaAndContentCreation}
+                                imageAlt="tjela"
+                                imageSrc="/images/tjela.jpg"
+                                year={2020}
+                            />
+                            <ProjectItem
+                                isActive
+                                isGrid
+                                isInPartnership
+                                brandLink="https://www.instagram.com/tasteofindiapt"
+                                brandTag="@tasteofindia"
+                                description={texts.socialMediaAndContentCreation}
+                                imageAlt="taste of india"
+                                imageSrc="/images/taste-of-india.jpg"
+                                year={2020}
+                            />
+                        </div>
+                        <div>
+                            <ProjectItem
+                                isActive
+                                isGrid
+                                brandLink="https://www.instagram.com/kaffeehaus_lisboa"
+                                brandTag="@kaffeehaus_lisboa"
+                                description={texts.socialMediaAndContentCreation}
+                                imageAlt="kaffeehaus"
+                                imageSrc="/images/kaffeehaus.jpg"
+                                year={2018}
+                            />
+                            <ProjectItem
+                                isActive
+                                isGrid
+                                isInPartnership
+                                brandLink="https://www.instagram.com/avocadohouselisbon"
+                                brandTag="@avocadohouselisbon"
+                                description={texts.socialMediaAndContentCreation}
+                                imageAlt="avocado"
+                                imageSrc="/images/avocado.jpg"
+                                year={2020}
+                            />
+                        </div>
+                        <div>
+                            <ProjectItem
+                                isActive
+                                isGrid
+                                isInPartnership
+                                brandLink="https://www.instagram.com/guacamolegmg"
+                                brandTag="@guacamolegmg"
+                                description={texts.socialMediaAndContentCreation}
+                                imageAlt="guacamole"
+                                imageSrc="/images/guacamole.jpg"
+                                year={2019}
+                            />
+                            <ProjectItem
+                                isGrid
+                                brandLink="https://industriacriativa.pt/projeto/45759/maria-limao-gestao-de-redes-sociais"
+                                brandTag="@bebemarialimao"
+                                description={texts.socialMediaAndContentCreation}
+                                imageAlt="marialimao"
+                                imageSrc="/images/maria-limao.jpg"
+                                year="2019 - 2020"
+                            />
+                        </div>
+                        <div>
+                            <ProjectItem
+                                isActive
+                                isGrid
+                                brandLink="https://www.instagram.com/jamesonportugal/"
+                                brandTag="@jamesonportugal"
+                                description={texts.socialMediaManagement}
+                                imageAlt="jameson"
+                                imageSrc="/images/jameson.jpg"
+                                year={2019}
+                            />
+                            <ProjectItem
+                                isActive
+                                isGrid
+                                brandLink="https://www.beefeater.pt"
+                                brandTag="@beefeater"
+                                description={texts.socialMediaManagement}
+                                imageAlt="beefeater"
+                                imageSrc="/images/beefeater.jpg"
+                                year={2019}
+                            />
+                        </div>
+                        <div>
+                            <ProjectItem
+                                isGrid
+                                brandLink="https://www.instagram.com/biergarten.jardimdacerveja"
+                                brandTag="@biergarten"
+                                description={texts.socialMediaAndContentCreation}
+                                imageAlt="biergarten"
+                                imageSrc="/images/biergarten.jpg"
+                                year={2019}
+                            />
+                            <ProjectItem
+                                isGrid
+                                brandLink="https://www.instagram.com/madmarycuisine"
+                                brandTag="@madmarycuisine"
+                                description={texts.socialMediaAndContentCreation}
+                                imageAlt="mad mary"
+                                imageSrc="/images/mad-mary.jpg"
+                                year={2019}
+                            />
+                        </div>
+                        <div>
+                            <ProjectItem
+                                isGrid
+                                brandLink="https://www.instagram.com/bovinelisboa"
+                                brandTag="@bovinelisboa"
+                                description={texts.socialMediaAndContentCreation}
+                                imageAlt="bovine"
+                                imageSrc="/images/bovine.jpg"
+                                year={2020}
+                            />
+                            <ProjectItem
+                                isGrid
+                                brandLink="https://www.instagram.com/icecreamroll.pt"
+                                brandTag="@icecreamroll.pt"
+                                description={texts.socialMediaAndContentCreation}
+                                imageAlt="icecream roll"
+                                imageSrc="/images/ice-cream-roll.jpg"
+                                year={2018}
+                            />
+                        </div>
+                        <div>
+                            <ProjectItem
+                                isGrid
+                                brandLink="https://www.instagram.com/ricemerestaurante"
+                                brandTag="@ricemerestaurante"
+                                description={texts.socialMediaAndContentCreation}
+                                imageAlt="rice me"
+                                imageSrc="/images/rice-me.jpg"
+                                year={2020}
+                            />
+                            <ProjectItem
+                                isGrid
+                                brandLink="https://www.instagram.com/ricemedeli"
+                                brandTag="@ricemedeli"
+                                description={texts.socialMediaAndContentCreation}
+                                imageAlt="rice me deli"
+                                imageSrc="/images/rice-me-deli.jpg"
+                                year={2020}
+                            />
+                        </div>
+                        <div>
+                            <ProjectItem
+                                isGrid
+                                brandLink="https://www.linkedin.com/company/harpoon-lda"
+                                brandTag="@harpoonjobs"
+                                description={texts.socialMediaAndContentCreation}
+                                imageAlt="harpoon"
+                                imageSrc="/images/harpoon.jpg"
+                                year="2020 - 2021"
+                            />
+                            <ProjectItem
+                                isGrid
+                                brandLink="https://www.instagram.com/catarinasantiago"
+                                brandTag="@catarinasantiago"
+                                description={texts.contentCreation}
+                                imageAlt="catarina santiago"
+                                imageSrc="/images/content-creator.jpg"
+                            />
+                        </div>
+                        <div>
+                            <ProjectItem
+                                isActive
+                                isGrid
+                                brandLink="https://instagram.com/4patasde5estrelas"
+                                brandTag="@4patasde5estrelas"
+                                description={texts.socialMediaManagement}
+                                imageAlt="4 patas de 5 estrelas"
+                                imageSrc="/images/4patas5estrelas.jpg"
+                                year={2020}
+                            />
+                            <ProjectItem
+                                isGrid
+                                isInPartnership
+                                brandLink="https://www.instagram.com/becomeluminouscoaching"
+                                brandTag="@becomeluminous"
+                                description={texts.socialMediaAndPaidSocial}
+                                imageAlt="luminous"
+                                imageSrc="/images/luminous.jpg"
+                                year={2020}
+                            />
+                        </div>
+                        <div>
+                            <ProjectItem
+                                isActive
+                                isGrid
+                                brandLink="https://industriacriativa.pt/projeto/14473/a-amiga-esteticista"
+                                brandTag="@aamigaesteticista"
+                                description={texts.consultingAndContentCreation}
+                                imageAlt="a amiga esteticista"
+                                imageSrc="/images/amiga-esteticista.jpg"
+                                year={2017}
+                            />
+                            <ProjectItem
+                                isGrid
+                                brandLink="https://industriacriativa.pt/projeto/45758/anaro-artist-content-creation"
+                                brandTag="@anaro.artistpage"
+                                description={texts.consultingAndContentCreation}
+                                imageAlt="AnaRo"
+                                imageSrc="/images/anaro.jpg"
+                                year={2019}
+                            />
+                        </div>
+                    </NukaCarousel>
+                </div>
+            </section>
+
+            <style jsx>
+                {`
+                    @import './styles/_vars.scss';
+
+                    .container {
+                        background: $white;
+
+                        .wrapper {
+                            display: flex;
+                            flex-direction: column;
+                            align-items: center;
+                        }
+                    }
+                `}
+            </style>
+        </>
+    );
+};
