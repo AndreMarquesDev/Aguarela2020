d186ee65409b06cdbc35b004a6c4eaefb92b6883
AndreMarquesDev <andregm_7@hotmail.com>
2021-12-21 23:55:07 +0000

    feat: Improve Header, FormField and LanguageButton unit tests

--- components/Header/Header.test.tsx
+++ components/Header/Header.test.tsx
@@ -1,5 +1,7 @@
+import '@testing-library/jest-dom';
 import React from 'react';
-import { render, RenderResult } from '@testing-library/react';
+import { render, RenderResult, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { Header, HeaderProps } from './Header';
 import NavLinksContext from '../context/NavLinksContext';
 import { Breakpoint } from '../../utils/useWindowSize';
@@ -10,10 +12,16 @@ const baseProps: HeaderProps = {
     language: 'pt',
 };
 
-const renderComponent = (newProps?: Partial<HeaderProps>): RenderResult => {
+const mockSetNavHeight = jest.fn();
+const mockToggleMenu = jest.fn();
+
+const renderComponent = (
+    setNavHeight: jest.Mock,
+    newProps?: Partial<HeaderProps>
+): RenderResult => {
     return render(
         <NavLinksContext.Provider
-            value={{ isMenuOpen: false, toggleMenu: jest.fn(), setNavHeight: jest.fn() }}
+            value={{ isMenuOpen: false, toggleMenu: mockToggleMenu, setNavHeight }}
         >
             <Header {...baseProps} {...newProps} />
         </NavLinksContext.Provider>
@@ -22,7 +30,9 @@ const renderComponent = (newProps?: Partial<HeaderProps>): RenderResult => {
 
 describe('<Header />', () => {
     test('renders properly', () => {
-        const { container } = renderComponent();
+        const { container } = renderComponent(mockSetNavHeight);
+
+        expect(mockSetNavHeight).toHaveBeenCalledTimes(2);
 
         expect(container).toMatchSnapshot();
     });
@@ -30,8 +40,28 @@ describe('<Header />', () => {
     test('renders properly below phablet', () => {
         setJestWindowWidth(Breakpoint.Mobile);
 
-        const { container } = renderComponent();
+        const { container } = renderComponent(mockSetNavHeight);
 
         expect(container).toMatchSnapshot();
+    });
+
+    test('does not call the "setNavHeight" function if it is undefined', () => {
+        renderComponent(undefined);
+
+        expect(mockSetNavHeight).toHaveBeenCalledTimes(0);
+    });
+
+    test('calls the "toggleMenu" function when clicking on <MenuIcon />', () => {
+        renderComponent(mockSetNavHeight);
+
+        const menuIcon = screen.getByRole('button');
+
+        expect(menuIcon).toBeInTheDocument();
+
+        expect(mockToggleMenu).toHaveBeenCalledTimes(0);
+
+        userEvent.click(menuIcon);
+
+        expect(mockToggleMenu).toHaveBeenCalledTimes(1);
     });
 });
--- components/LanguageButton/LanguageButton.test.tsx
+++ components/LanguageButton/LanguageButton.test.tsx
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom';
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, RenderResult, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import * as nextRouter from 'next/router';
 import { LanguageButton } from './LanguageButton';
@@ -14,13 +14,19 @@ nextRouter.useRouter = jest.fn(() => ({
     replace: jest.fn(),
 }));
 
+const mockToggleMenu = jest.fn();
+
+const renderComponent = (isMenuOpen = true): RenderResult => {
+    return render(
+        <NavLinksContext.Provider value={{ isMenuOpen, toggleMenu: mockToggleMenu }}>
+            <LanguageButton />
+        </NavLinksContext.Provider>
+    );
+};
+
 describe('<LanguageButton />', () => {
     test('renders properly', () => {
-        const { container } = render(
-            <NavLinksContext.Provider value={{ isMenuOpen: false, toggleMenu: jest.fn() }}>
-                <LanguageButton />
-            </NavLinksContext.Provider>
-        );
+        const { container } = renderComponent();
 
         const button = screen.getByRole('button');
 
@@ -34,21 +40,32 @@ describe('<LanguageButton />', () => {
             value: {
                 pathname: '/en/about',
             },
-            writable: true,
         });
 
-        render(
-            <NavLinksContext.Provider value={{ isMenuOpen: false, toggleMenu: jest.fn() }}>
-                <LanguageButton />
-            </NavLinksContext.Provider>
-        );
+        renderComponent();
 
         const button = screen.getByText('pt');
 
         expect(button).toBeInTheDocument();
 
+        expect(mockToggleMenu).toHaveBeenCalledTimes(0);
+
         userEvent.click(button);
 
         expect(document.title).toBe('Sobre - Aguarela Digital');
+
+        expect(mockToggleMenu).toHaveBeenCalledTimes(1);
+    });
+
+    test('does not call the "toggleMenu" function if menu is closed', () => {
+        renderComponent(false);
+
+        const button = screen.getByText('pt');
+
+        expect(mockToggleMenu).toHaveBeenCalledTimes(0);
+
+        userEvent.click(button);
+
+        expect(mockToggleMenu).toHaveBeenCalledTimes(0);
     });
 });
