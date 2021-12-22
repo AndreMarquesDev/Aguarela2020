4a62fa50475b163c5411397e7198261566f00874
AndreMarquesDev <andregm_7@hotmail.com>
2021-12-22 12:47:37 +0000

    feat: Improve Layout unit test

--- components/Layout/Layout.test.tsx
+++ components/Layout/Layout.test.tsx
@@ -1,10 +1,20 @@
 import '@testing-library/jest-dom';
 import React from 'react';
 import { render, screen, RenderResult } from '@testing-library/react';
+import * as nextRouter from 'next/router';
 import { Layout, LayoutProps } from './Layout';
 import NavLinksContext from '../context/NavLinksContext';
 import { Breakpoint } from '../../utils/useWindowSize';
 import { setJestWindowWidth } from '../../utils/jest/setJestWindowWidth';
+import { textsEn } from '../../utils/texts';
+
+// @ts-ignore
+nextRouter.useRouter = jest.fn(() => ({
+    route: '/',
+    query: {
+        language: 'pt',
+    },
+}));
 
 const baseProps: LayoutProps = {
     children: (
@@ -39,6 +49,23 @@ describe('<Layout />', () => {
         setJestWindowWidth(Breakpoint.Mobile);
 
         const { container } = renderComponent({}, true);
+
+        expect(container).toMatchSnapshot();
+    });
+
+    test('renders properly in English', () => {
+        (nextRouter.useRouter as jest.Mock).mockImplementation(() => ({
+            route: '/',
+            query: {
+                language: 'en',
+            },
+        }));
+
+        const { container } = renderComponent();
+
+        const aboutLink = screen.getByText(textsEn.about);
+
+        expect(aboutLink).toBeInTheDocument();
 
         expect(container).toMatchSnapshot();
     });
