20edda6435889abf7110827107963b410f3142cb
AndreMarquesDev <andregm_7@hotmail.com>
2021-12-26 23:47:40 +0000

    feat: Add initial Cypress setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
-    extends: ['andremarquesdev', 'plugin:jest/recommended', 'plugin:testing-library/react'],
-    plugins: ['jest', 'testing-library'],
+    extends: [
+        'andremarquesdev',
+        'plugin:jest/recommended',
+        'plugin:testing-library/react',
+        'plugin:cypress/recommended',
+    ],
+    plugins: ['jest', 'testing-library', 'cypress'],
 };
