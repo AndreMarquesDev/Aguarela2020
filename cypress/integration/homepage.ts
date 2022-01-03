ea4790fd155a7dd3b85d83870d9356b5e9bf6fe9
AndreMarquesDev <andregm_7@hotmail.com>
2022-01-03 23:46:16 +0000

    feat: Add homepage workflow and projects blocks tests

--- cypress/integration/homepage.ts
+++ cypress/integration/homepage.ts
@@ -1,3 +1,7 @@
+import {
+    projectsListNoCarouselDataTestId,
+    skillsBlockItemWrapperDataTestId,
+} from '../../utils/dataTestIds';
 import { textsPt } from '../../utils/texts';
 import { urls } from '../utils/selectors';
 
@@ -13,7 +17,7 @@ describe('homepage', () => {
     });
 
     it('loads banner', () => {
-        cy.imageIsVisible('homepage banner');
+        cy.imageIsVisible('.homepageBanner', 'homepage banner');
     });
 
     it('loads the welcome block', () => {
@@ -29,28 +33,143 @@ describe('homepage', () => {
     it('loads the skills block', () => {
         cy.contains(textsPt.skills).scrollIntoView();
 
-        cy.imageIsVisible(textsPt.socialMediaStrategy);
+        cy.imageIsVisible(
+            `[data-testid="${skillsBlockItemWrapperDataTestId}"]`,
+            textsPt.socialMediaStrategy
+        );
         cy.contains(textsPt.socialMediaStrategy);
         cy.contains(textsPt.makingAuditsAndAnalysis);
 
-        cy.imageIsVisible(textsPt.socialMediaConsulting);
+        cy.imageIsVisible(
+            `[data-testid="${skillsBlockItemWrapperDataTestId}"]`,
+            textsPt.socialMediaConsulting
+        );
         cy.contains(textsPt.socialMediaConsulting);
         cy.contains(textsPt.weCanHelpYourTeam);
 
-        cy.imageIsVisible(textsPt.communityManagement);
+        cy.imageIsVisible(
+            `[data-testid="${skillsBlockItemWrapperDataTestId}"]`,
+            textsPt.communityManagement
+        );
         cy.contains(textsPt.communityManagement);
         cy.contains(textsPt.whenWeSendAMessage);
 
-        cy.imageIsVisible(textsPt.paidSocialAndSearch);
+        cy.imageIsVisible(
+            `[data-testid="${skillsBlockItemWrapperDataTestId}"]`,
+            textsPt.paidSocialAndSearch
+        );
         cy.contains(textsPt.paidSocialAndSearch);
         cy.contains(textsPt.planningAndImplementing);
 
-        cy.imageIsVisible(textsPt.optimizationAndAnalysis);
+        cy.imageIsVisible(
+            `[data-testid="${skillsBlockItemWrapperDataTestId}"]`,
+            textsPt.optimizationAndAnalysis
+        );
         cy.contains(textsPt.optimizationAndAnalysis);
         cy.contains(textsPt.measuringResults);
 
-        cy.imageIsVisible(textsPt.contentCreation);
+        cy.imageIsVisible(
+            `[data-testid="${skillsBlockItemWrapperDataTestId}"]`,
+            textsPt.contentCreation
+        );
         cy.contains(textsPt.contentCreation);
         cy.contains(textsPt.createAttractiveContent);
+    });
+
+    it('loads the workflow block', () => {
+        cy.contains(textsPt.workflow).scrollIntoView();
+
+        cy.contains(textsPt.defineTarget);
+        cy.contains(textsPt.whatIsTheTarget).should('be.hidden');
+        cy.contains(textsPt.defineTarget).parent().find('.backface').invoke('show');
+        cy.contains(textsPt.whatIsTheTarget).should('be.visible');
+
+        cy.contains(textsPt.defineGoals);
+        cy.contains(textsPt.whatAreYourGoals).should('be.hidden');
+        cy.contains(textsPt.defineGoals).parent().find('.backface').invoke('show');
+        cy.contains(textsPt.whatAreYourGoals).should('be.visible');
+
+        cy.contains(textsPt.platformStrategy);
+        cy.contains(textsPt.whatPlatforms).should('be.hidden');
+        cy.contains(textsPt.platformStrategy).parent().find('.backface').invoke('show');
+        cy.contains(textsPt.whatPlatforms).should('be.visible');
+
+        cy.contains(textsPt.workflow).next().contains(textsPt.contentCreation);
+        cy.contains(textsPt.relevantContentCreation).should('be.hidden');
+        cy.contains(textsPt.workflow)
+            .next()
+            .contains(textsPt.contentCreation)
+            .parent()
+            .find('.backface')
+            .invoke('show');
+        cy.contains(textsPt.relevantContentCreation).should('be.visible');
+
+        cy.contains(textsPt.paidSocialAndInfluencers);
+        cy.contains(textsPt.promotePostsAndCollaborate).should('be.hidden');
+        cy.contains(textsPt.paidSocialAndInfluencers).parent().find('.backface').invoke('show');
+        cy.contains(textsPt.promotePostsAndCollaborate).should('be.visible');
+
+        cy.contains(textsPt.insightsAndReports);
+        cy.contains(textsPt.detailedAnalysis).should('be.hidden');
+        cy.contains(textsPt.insightsAndReports).parent().find('.backface').invoke('show');
+        cy.contains(textsPt.detailedAnalysis).should('be.visible');
+
+        cy.contains(textsPt.workflow).next().contains(textsPt.optimization);
+        cy.contains(textsPt.improvementsAndUpdatesToTheStrategy).should('be.hidden');
+        cy.contains(textsPt.workflow)
+            .next()
+            .contains(textsPt.optimization)
+            .parent()
+            .find('.backface')
+            .invoke('show');
+        cy.contains(textsPt.improvementsAndUpdatesToTheStrategy).should('be.visible');
+    });
+
+    it('loads the projects block', () => {
+        const ulSelector = `[data-testid="${projectsListNoCarouselDataTestId}"]`;
+
+        cy.contains('section', textsPt.projects).scrollIntoView();
+
+        cy.imageIsVisible(ulSelector, 'tjela logo');
+        cy.get(ulSelector).contains('@tudonatjela').should('be.hidden');
+        cy.get(`${ulSelector} [alt="tjela logo"]`)
+            .parentsUntil('ul', 'li')
+            .find('.backface')
+            .invoke('show');
+        cy.get(ulSelector).contains('@tudonatjela').should('be.visible');
+        cy.get(ulSelector)
+            .contains(textsPt.socialMediaManagementAndContentCreation)
+            .should('be.visible');
+        cy.get(ulSelector).contains(`2020 - ${textsPt.present}`).should('be.visible');
+        cy.get(ulSelector).contains(`* ${textsPt.inPartnershipWith}`).should('be.visible');
+
+        cy.imageIsVisible(ulSelector, 'kaffeehaus logo');
+        cy.get(ulSelector).contains('@kaffeehaus_lisboa').should('be.hidden');
+        cy.get(`${ulSelector} [alt="kaffeehaus logo"]`)
+            .parentsUntil('ul', 'li')
+            .find('.backface')
+            .invoke('show');
+        cy.get(ulSelector).contains('@kaffeehaus_lisboa').should('be.visible');
+        cy.get(ulSelector)
+            .contains(textsPt.socialMediaManagementAndContentCreation)
+            .should('be.visible');
+        cy.get(ulSelector).contains(`2018 - ${textsPt.present}`).should('be.visible');
+
+        cy.imageIsVisible(ulSelector, 'guacamole logo');
+        cy.get(ulSelector).contains('@guacamolegmg').should('be.hidden');
+        cy.get(`${ulSelector} [alt="guacamole logo"]`)
+            .parentsUntil('ul', 'li')
+            .find('.backface')
+            .invoke('show');
+        cy.get(ulSelector).contains('@guacamolegmg').should('be.visible');
+        cy.get(ulSelector)
+            .contains(textsPt.socialMediaManagementAndContentCreation)
+            .should('be.visible');
+        cy.get(ulSelector).contains(`2019 - ${textsPt.present}`).should('be.visible');
+        cy.get(ulSelector).contains(`* ${textsPt.inPartnershipWith}`).should('be.visible');
+
+        cy.contains(textsPt.seeMore).click();
+
+        cy.url().should('eq', urls.pt.homepage);
     });
 });
