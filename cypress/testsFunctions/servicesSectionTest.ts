980ea3987e29e9fb248d5209a2d1ba19d19cc59e
AndreMarquesDev <andregm_7@hotmail.com>
2022-01-10 23:07:25 +0000

    feat: Add services page test

---
+++ cypress/testsFunctions/servicesSectionTest.ts
@@ -0,0 +1,56 @@
+import {
+    servicesBlockSectionDataTestId,
+    servicesBlockItemWrapperDataTestId,
+} from '../../utils/dataTestIds';
+import { Locale } from '../../utils/locales';
+import { getLocalizedTexts } from '../utils/getTexts';
+
+export const servicesSectionTest = (locale: Locale): void => {
+    const {
+        services,
+        design,
+        webDesign,
+        outdoorsAndFlyers,
+        contactCard,
+        socialMediaTemplates,
+        menus,
+        socialMediaEn,
+        contentCreation,
+        marketAnalysis,
+        socialMediaStrategyAndManagement,
+        paidSocial,
+        consulting,
+        digital,
+        paidSearchCampaigns,
+        opinionPlatformsManagement,
+    } = getLocalizedTexts(locale);
+
+    cy.getByText(servicesBlockSectionDataTestId, services);
+
+    cy.getByText(servicesBlockSectionDataTestId, design);
+    cy.isHidden(servicesBlockSectionDataTestId, `- ${webDesign}`);
+    cy.getByText(servicesBlockSectionDataTestId, design).next().forceHover();
+    cy.isVisible(servicesBlockSectionDataTestId, `- ${webDesign}`);
+    cy.isVisible(servicesBlockSectionDataTestId, `- ${outdoorsAndFlyers}`);
+    cy.isVisible(servicesBlockSectionDataTestId, `- ${contactCard}`);
+    cy.isVisible(servicesBlockSectionDataTestId, `- ${socialMediaTemplates}`);
+    cy.isVisible(servicesBlockSectionDataTestId, `- ${menus}`);
+
+    cy.get(`[data-testid="${servicesBlockItemWrapperDataTestId}"] > p`).contains(socialMediaEn);
+    cy.isHidden(servicesBlockSectionDataTestId, `- ${contentCreation}`);
+    cy.get(`[data-testid="${servicesBlockItemWrapperDataTestId}"] > p`)
+        .contains(socialMediaEn)
+        .next()
+        .forceHover();
+    cy.isVisible(servicesBlockSectionDataTestId, `- ${contentCreation}`);
+    cy.isVisible(servicesBlockSectionDataTestId, `- ${marketAnalysis}`);
+    cy.isVisible(servicesBlockSectionDataTestId, `- ${socialMediaStrategyAndManagement}`);
+    cy.isVisible(servicesBlockSectionDataTestId, `- ${paidSocial}`);
+    cy.isVisible(servicesBlockSectionDataTestId, `- ${consulting}`);
+
+    cy.getByText(servicesBlockSectionDataTestId, digital);
+    cy.isHidden(servicesBlockSectionDataTestId, `- ${paidSearchCampaigns}`);
+    cy.getByText(servicesBlockSectionDataTestId, digital).next().forceHover();
+    cy.isVisible(servicesBlockSectionDataTestId, `- ${paidSearchCampaigns}`);
+    cy.isVisible(servicesBlockSectionDataTestId, `- ${opinionPlatformsManagement}`);
+};
