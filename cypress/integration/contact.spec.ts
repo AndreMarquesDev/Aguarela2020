e7d93b3c2a8695ae6d4b4321b24aa3fd4fe9a8b9
AndreMarquesDev <andregm_7@hotmail.com>
2022-03-21 17:48:21 +0000

    fix: Fix cypress tests failing in CI

---
+++ cypress/integration/contact.spec.ts
@@ -0,0 +1,41 @@
+import { Locale } from '../../utils/locales';
+import { contactBlockTest } from '../testsFunctions/contactBlockTest';
+import { contactFormTest } from '../testsFunctions/contactFormTest';
+import { footerTest } from '../testsFunctions/footerTest';
+import { navigationContactPageTest } from '../testsFunctions/navigation/navigationContactPageTest';
+import { urls } from '../utils/selectors';
+import { defaultViewportWidth, defaultViewportHeight } from '../utils/variables';
+
+const locale: Locale = 'pt';
+const otherLocale: Locale = 'en';
+const pageBeingTested = urls.pt.contact;
+
+describe('contact page', () => {
+    beforeEach(() => {
+        cy.visit(pageBeingTested);
+
+        cy.viewport(defaultViewportWidth, defaultViewportHeight);
+
+        cy.document().its('fonts').invoke('check', '900 150rem Montserrat').should('be.true');
+    });
+
+    it('loads', () => {
+        cy.urlIsEqualTo(pageBeingTested);
+    });
+
+    it('renders the header and navigates properly', () => {
+        navigationContactPageTest(locale, otherLocale, pageBeingTested);
+    });
+
+    it('renders the contact block', () => {
+        contactBlockTest(locale);
+    });
+
+    it('renders the contact form', () => {
+        contactFormTest(locale);
+    });
+
+    it('renders the footer', () => {
+        footerTest(locale, pageBeingTested);
+    });
+});
--- jest.config.ts
+++ jest.config.ts
@@ -82,7 +82,7 @@ export default {
     // moduleNameMapper: {},
 
     // an array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
-    // modulePathIgnorePatterns: [],
+    modulePathIgnorePatterns: ['cypress'],
 
     // activates notifications for test results
     // notify: false,
