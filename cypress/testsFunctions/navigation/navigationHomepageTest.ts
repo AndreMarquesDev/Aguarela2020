dade764ee63d88f837876e9f7fad12f1dbe0dbf8
AndreMarquesDev <andregm_7@hotmail.com>
2022-04-11 15:53:36 +0100

    chore: Add cypress firefox tests

--- cypress/testsFunctions/navigation/navigationHomepageTest.ts
+++ cypress/testsFunctions/navigation/navigationHomepageTest.ts
@@ -18,44 +18,45 @@ export const navigationHomepageTest = (
 ): void => {
     const { about, projects, services, contact } = getLocalizedTexts(locale);
     const remainingTexts = getLocalizedTexts(otherLocale);
+    const isFirefox = Cypress.isBrowser('firefox');
 
-    cy.imageIsVisible(headerDataTestId, 'Logo');
+    cy.imageIsVisible(headerDataTestId, 'Logo', isFirefox);
 
-    cy.getByText(headerDataTestId, about).click();
+    cy.getByText(headerDataTestId, about).click({ force: isFirefox });
     cy.getByDataTestId(aboutMeSectionDataTestId);
     cy.urlIsEqualTo(urls[locale].about);
-    cy.getByDataTestId(homepageLogoLinkDataTestId).click();
+    cy.getByDataTestId(homepageLogoLinkDataTestId).click({ force: isFirefox });
     cy.getByDataTestId(homepageBannerContainerDataTestId);
     cy.urlIsEqualTo(pageBeingTested);
 
-    cy.getByText(headerDataTestId, projects).click();
+    cy.getByText(headerDataTestId, projects).click({ force: isFirefox });
     cy.getByDataTestId(projectsListDoubleSectionDataTestId);
     cy.urlIsEqualTo(urls[locale].projects);
-    cy.getByDataTestId(homepageLogoLinkDataTestId).click();
+    cy.getByDataTestId(homepageLogoLinkDataTestId).click({ force: isFirefox });
     cy.getByDataTestId(homepageBannerContainerDataTestId);
     cy.urlIsEqualTo(pageBeingTested);
 
-    cy.getByText(headerDataTestId, services).click();
+    cy.getByText(headerDataTestId, services).click({ force: isFirefox });
     cy.getByDataTestId(servicesBlockSectionDataTestId);
     cy.urlIsEqualTo(urls[locale].services);
-    cy.getByDataTestId(homepageLogoLinkDataTestId).click();
+    cy.getByDataTestId(homepageLogoLinkDataTestId).click({ force: isFirefox });
     cy.getByDataTestId(homepageBannerContainerDataTestId);
     cy.urlIsEqualTo(pageBeingTested);
 
-    cy.getByText(headerDataTestId, contact).click();
+    cy.getByText(headerDataTestId, contact).click({ force: isFirefox });
     cy.getByDataTestId(contactBlockSectionDataTestId);
     cy.urlIsEqualTo(urls[locale].contact);
-    cy.getByDataTestId(homepageLogoLinkDataTestId).click();
+    cy.getByDataTestId(homepageLogoLinkDataTestId).click({ force: isFirefox });
     cy.getByDataTestId(homepageBannerContainerDataTestId);
     cy.urlIsEqualTo(pageBeingTested);
 
-    cy.getByText(headerDataTestId, otherLocale).click();
+    cy.getByText(headerDataTestId, otherLocale).click({ force: isFirefox });
     cy.getByText(headerDataTestId, remainingTexts.about);
     cy.getByText(headerDataTestId, remainingTexts.projects);
     cy.getByText(headerDataTestId, remainingTexts.services);
     cy.getByText(headerDataTestId, remainingTexts.contact);
     cy.urlIsEqualTo(urls[otherLocale].homepage);
-    cy.getByText(headerDataTestId, locale).click();
+    cy.getByText(headerDataTestId, locale).click({ force: isFirefox });
     cy.getByText(headerDataTestId, about);
     cy.getByText(headerDataTestId, projects);
     cy.getByText(headerDataTestId, services);
