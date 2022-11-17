71fc655dc40d8f43c448a9e199e26810e6e39858
AndreMarquesDev <andregm_7@hotmail.com>
2022-11-17 01:06:40 +0000

    feat: Add LogRocket

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,17 @@ import React from 'react';
 import '../styles/main.scss';
 import Cookies from 'js-cookie';
 import { getInitialLocale } from 'multilingual-url/lib';
+import LogRocket from 'logrocket';
 import { defaultLocale, locales } from '../constants/locales';
 import { initializeAxiosMockAdapter } from '../ajax/axiosMockAdapter';
 
 function MyApp({ Component, pageProps }) {
     const hasLangCookie = Cookies.get('lang');
     const locale = getInitialLocale(defaultLocale, locales);
+    const hasDocument = typeof document !== 'undefined';
+    const isCypress = typeof window !== 'undefined' && window.Cypress;
+    const isProd = process.env.NODE_ENV === 'production';
+    const isDev = process.env.NODE_ENV === 'development';
 
     if (!hasLangCookie) {
         Cookies.set('lang', locale, {
@@ -16,13 +21,16 @@ function MyApp({ Component, pageProps }) {
         });
     }
 
-    const hasDocument = typeof document !== 'undefined';
-
     if (hasDocument) {
         document.querySelector('html').lang = locale; // eslint-disable-line no-undef
     }
 
-    if (process.env.NODE_ENV === 'development') {
+    if (isProd && !isCypress) {
+        LogRocket.init(process.env.NEXT_PUBLIC_LOGROCKET_APPID);
+        LogRocket.identify();
+    }
+
+    if (isDev) {
         initializeAxiosMockAdapter(1000);
     }
 
