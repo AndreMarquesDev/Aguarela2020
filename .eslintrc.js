8f0d8f7a0f709968374adaac38ff04ea2f39d126
AndreMarquesDev <andregm_7@hotmail.com>
2022-12-20 01:34:25 +0000

    feat: Setup Playwright config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
     extends: ['andremarquesdev', 'plugin:cypress/recommended'],
     plugins: ['cypress'],
     parserOptions: {
-        project: ['tsconfig.json', './cypress/tsconfig.json'],
+        project: ['tsconfig.json', './cypress/tsconfig.json', './e2e/tsconfig.json'],
     },
     ignorePatterns: ['!.*'],
 };
