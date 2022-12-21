e292c95199c146d78bbdaaf6fc796c0f18f6b88c
AndreMarquesDev <andregm_7@hotmail.com>
2022-12-21 00:59:19 +0000

    feat: Create lets work and footer tests

---
+++ e2e/testBlocks/letsWorkSectionTest.ts
@@ -0,0 +1,23 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Page, expect } from '@playwright/test';
+import { Locale } from '../../types/Locale';
+import { letsWorkSectionDataTestId } from '../../utils/dataTestIds';
+import { getLocalizedTexts } from '../utils/utils';
+import { urls } from '../utils/selectors';
+
+export const letsWorkSectionTest = async (page: Page, locale: Locale): Promise<void> => {
+    const { letsWork, letsWorkDescription, contact } = getLocalizedTexts(locale);
+    const container = page.getByTestId(letsWorkSectionDataTestId);
+    const anchor = container.getByRole('link', { name: contact });
+
+    // renders section title
+    await expect(container.getByText(letsWork)).toBeVisible();
+
+    // renders text block
+    await expect(container.getByText(letsWorkDescription)).toBeVisible();
+
+    // click on button redirects to contact page
+    await expect(anchor).toBeVisible();
+    await anchor.click();
+    await expect(page).toHaveURL(urls[locale].contact);
+};
--- e2e/utils/utils.ts
+++ e2e/utils/utils.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { Page } from '@playwright/test';
+import { Locator, Page, expect } from '@playwright/test';
 import { Locale } from '../../types/Locale';
 import { TextsInterface, textsPt, textsEn } from '../../utils/texts';
 
@@ -15,4 +15,23 @@ export const openMenuMobile = (page: Page, isMobile: boolean): void => {
     if (isMobile) {
         page.getByRole('button', { name: 'toggle menu' }).click();
     }
+};
+
+export const openNewTab = async (
+    page: Page,
+    anchor: Locator,
+    url: string | RegExp,
+    timeout = 5000
+): Promise<void> => {
+    await expect(anchor).toBeVisible();
+
+    const newTabPromise = page.waitForEvent('popup');
+
+    await anchor.click();
+
+    const newTab = await newTabPromise;
+
+    await newTab.waitForLoadState();
+
+    await expect(newTab).toHaveURL(url, { timeout });
 };
