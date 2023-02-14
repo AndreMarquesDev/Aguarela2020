d711a7bed7d599ecf6c8995127de229db369dea5
AndreMarquesDev <andregm_7@hotmail.com>
2023-02-14 21:08:38 +0000

    feat: Create contact page spec

---
+++ e2e/testBlocks/contactBlockTest.ts
@@ -0,0 +1,43 @@
+import { Page, expect } from '@playwright/test';
+import { Locale } from '../../types/Locale';
+import { contactBlockSectionDataTestId } from '../../utils/dataTestIds';
+import { getLocalizedTexts } from '../utils/utils';
+import { aguarelaDigitalEmail } from '../../utils/urls';
+
+export const contactBlockTest = async (
+    page: Page,
+    isMobile: boolean,
+    locale: Locale
+): Promise<void> => {
+    const {
+        contactMe1,
+        contactMe2,
+        contactMe3,
+        needHelpWithYourBusiness,
+        sendMeAnEmail,
+        iAmAvailableToAdvise,
+    } = getLocalizedTexts(locale);
+    const container = page.getByTestId(contactBlockSectionDataTestId);
+    const anchor = container.getByRole('link', { name: aguarelaDigitalEmail });
+
+    // renders section title
+    // on mobile it's only one element but in desktop it is split into 3 different elements
+    if (isMobile) {
+        await expect(
+            container.getByText(`${contactMe1}${contactMe2}${contactMe3}`, { exact: true })
+        ).toBeVisible();
+    } else {
+        await expect(container.getByText(contactMe1, { exact: true })).toBeVisible();
+        await expect(container.getByText(contactMe2, { exact: true })).toBeVisible();
+        await expect(container.getByText(contactMe3, { exact: true })).toBeVisible();
+    }
+
+    // renders text block
+    await expect(container.getByText(needHelpWithYourBusiness)).toBeVisible();
+    await expect(container.getByText(sendMeAnEmail)).toBeVisible();
+    await expect(container.getByText(iAmAvailableToAdvise)).toBeVisible();
+
+    // button has a 'mailto' link
+    await expect(anchor).toBeVisible();
+    await expect(anchor).toHaveAttribute('href', `mailto:${aguarelaDigitalEmail}`);
+};
