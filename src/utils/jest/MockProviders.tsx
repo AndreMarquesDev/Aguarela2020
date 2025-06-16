04efbb82b7c54f9e2f7d2c3d58e4f7eb76ebdf8a
AndreMarquesDev <andregm_7@hotmail.com>
2025-06-16 23:01:43 +0100

    chore: manual fixes

--- src/utils/jest/MockProviders.tsx
+++ src/utils/jest/MockProviders.tsx
@@ -1,31 +1,35 @@
-/* eslint-disable react/jsx-no-constructed-context-values */
-import React, { ReactNode } from 'react';
+/* eslint-disable unicorn/filename-case */
+import type { ReactNode } from 'react';
+import React from 'react';
 import { NavLinksContext } from '../../components/context/NavLinksContext';
 import { TextsContext } from '../../components/context/TextsContext';
 import { Locale } from '../../types/Locale';
 import { textsEn, textsPt } from '../texts';
 
-interface MockProvidersProps {
+type MockProvidersProps = {
     language?: Locale;
     isMenuOpen?: boolean;
     toggleMenu?: jest.Mock;
     setNavHeight?: jest.Mock;
     children: ReactNode;
-}
+};
 
 export const MockProviders = ({
     language = Locale.Pt,
     isMenuOpen = false,
+    // eslint-disable-next-line react/no-unstable-default-props
     toggleMenu = jest.fn(),
     setNavHeight = undefined,
     children,
 }: MockProvidersProps): JSX.Element => {
     return (
         <TextsContext.Provider
+            // eslint-disable-next-line react/no-unstable-context-value
             value={{
                 texts: language === Locale.Pt ? textsPt : textsEn,
             }}
         >
+            {/* eslint-disable-next-line react/no-unstable-context-value */}
             <NavLinksContext.Provider value={{ isMenuOpen, toggleMenu, setNavHeight }}>
                 {children}
             </NavLinksContext.Provider>
